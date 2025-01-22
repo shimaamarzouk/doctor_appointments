e2de60d8514f576612b17ce942f40153144cda7f
OmarElsorogy <gneomar2021@gmail.com>
2025-01-23 00:27:05 +0300

    Fixed slots repo issue

--- config/providers/appointment_booking.rb
+++ config/providers/appointment_booking.rb
@@ -6,13 +6,15 @@ Hanami.app.register_provider :appointment_booking do
     require_relative "../../slices/appointment_booking/infrastructure/repositories/rom_appointment_repository"
     require_relative "../../slices/appointment_booking/application/use_cases/book_appointment"
     require_relative "../../slices/appointment_booking/application/use_cases/list_available_slots"
+    require_relative "../../slices/doctor_availability/repositories/slot_repository"
   end
 
   start do
     appointment_repository = AppointmentBooking::Infrastructure::Repositories::RomAppointmentRepository.new
-    slot_repository = Hanami.app["repositories.slot_repository"]
+    slot_repository = DoctorAvailability::Repositories::SlotRepository.new  # Updated reference
 
     register "repositories.appointment_repository", appointment_repository
+    register "repositories.slot_repository", slot_repository  # Add explicit registration
     register "use_cases.book_appointment", 
       AppointmentBooking::Application::UseCases::BookAppointment.new(
         appointment_repository: appointment_repository,
